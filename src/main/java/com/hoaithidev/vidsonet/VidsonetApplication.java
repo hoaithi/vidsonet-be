673e987eddc5fba3cd2b15d97add1a9e692c7023
hoaithi <tranthi2965@gmail.com>
2025-02-24 16:02:24 +0700

    first commit

---
+++ src/main/java/com/hoaithidev/vidsonet/VidsonetApplication.java
@@ -0,0 +1,13 @@
+package com.hoaithidev.vidsonet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidsonetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidsonetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoaithidev/vidsonet/VidsonetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoaithidev.vidsonet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidsonetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
